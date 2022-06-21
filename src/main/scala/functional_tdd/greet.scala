f0b165727162c5975fe8d7634d440e507808096a
yasuabe2613 <yasu@yasuabe.net>
2022-06-22 00:02:33 +0900

    add functional TDD sample code

---
+++ src/main/scala/functional_tdd/greet.scala
@@ -0,0 +1,45 @@
+package functional_tdd
+
+import java.time.LocalTime
+import cats.Functor
+import cats.syntax.functor.*
+import cats.syntax.flatMap.*
+import cats.effect.{ ExitCode, IO, IOApp }
+
+// https://qiita.com/yasuabe2613/items/fbdd139f00161ff3186d
+
+trait Now[F[_]]:
+  def time: F[LocalTime]
+
+object Now:
+  def apply[F[_]](using N: Now[F]) = N
+
+object NowDemo extends IOApp.Simple:
+  given Now[IO] with
+    def time: IO[LocalTime] = IO.delay(LocalTime.now)
+
+  def run: IO[Unit] = Now[IO].time >>= IO.println
+
+object greet:
+  // 参照等価な部分
+  def greetingOf(t: LocalTime): String =
+    val before = (h: Int, m: Int, s: Int) => t isBefore LocalTime.of(h, m, s)
+
+    if      before( 5, 0, 0) then "こんばんは"
+    else if before(12, 0, 0) then "おはようございます"
+    else if before(18, 0, 0) then "こんにちは"
+    else                          "こんばんは"
+
+  // 参照等価な greetingOf と、参照透過じゃない Now.time の合成
+  def greetNow[F[_]: Functor: Now]: F[String] =
+    Now[F].time map greetingOf
+
+object GreeterDemo extends IOApp.Simple:
+  import greet._
+
+  given Now[IO] with
+    def time: IO[LocalTime] = IO.delay(LocalTime.now)
+
+  def run: IO[Unit] = 
+    ((Now[IO].time map greetingOf) >>= IO.println) >>
+    (greetNow[IO]                  >>= IO.println)
---
+++ src/test/scala/functional_tdd/GreetSuite.scala
@@ -0,0 +1,45 @@
+package functional_tdd
+
+import java.time.LocalTime
+import cats.Id
+import cats.syntax.apply.*
+import cats.syntax.functor.*
+import munit.ScalaCheckSuite
+import org.scalacheck.{ Arbitrary, Gen }
+import org.scalacheck.Prop.*
+import org.scalacheck.cats.implicits.*
+
+class GreetSuit extends ScalaCheckSuite:
+  import greet._
+
+  test("与えられた時刻があいさつに正しく対応付けられる") {
+    List(
+      // 時 | 分 | 秒 | ナノ      | 挨拶
+      ((  0 ,  0 ,  0 ,         0), "こんばんは"        ),
+      ((  4 , 59 , 59 , 999999999), "こんばんは"        ),
+      ((  5 ,  0 ,  0 ,         0), "おはようございます"),
+      (( 11 , 59 , 59 , 999999999), "おはようございます"),
+      (( 12 ,  0 ,  0 ,         0), "こんにちは"        ),
+      (( 17 , 59 , 59 , 999999999), "こんにちは"        ),
+      (( 18 ,  0 ,  0 ,         0), "こんばんは"        ),
+      (( 23 , 59 , 59 , 999999999), "こんばんは"        ),
+    ) foreach { case ((h, m, s, n), expected) =>
+      val t = LocalTime.of(h, m, s, n)
+      assertEquals(greetingOf(t), (expected))
+    }
+  }
+  property("現在時刻があいさつに正しく対応付けられる") {
+    given Arbitrary[LocalTime] = Arbitrary {
+      val hourGen = Gen.choose(0, 23)
+      val minGen  = Gen.choose(0, 59)
+      val secGen  = Gen.choose(0, 59)
+      val nanoGen = Gen.choose(0, 999999999)
+      (hourGen, minGen, secGen, nanoGen) mapN LocalTime.of
+    }
+    forAll { (t: LocalTime) =>
+      given Now[Id] with
+        def time: Id[LocalTime] = t
+
+      greetNow[Id] == greetingOf(t)
+    }
+  }
